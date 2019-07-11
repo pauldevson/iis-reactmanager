5050379bc334b286867e2a0223867231545bd631
Sanchez Cervantes Paul Alexis (RBEI/EBS1.6.2-MX) <paul.sanchezcervantes@mx.bosch.com>
2019-07-11 17:41:55 -0500

    Adding theme settings

--- src/AppLayout.js
+++ src/AppLayout.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import teal from '@material-ui/core/colors/teal';
 import { Root, config } from 'Layout';
 import Home from 'components/Home';
 import NotFound from 'components/NotFound';
 import Connect from 'components/Connect';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: teal
+  }
+});
+
 const App = () => (
-  <Root config={config} style={{ minHeight: '100vh' }}>
-    <CssBaseline />
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/get" component={Home} />
-        <Route path="/connect" component={Connect} />
-        <Route path="/server/websites" component={Connect} />
-        <Route component={NotFound} />
-      </Switch>
-    </BrowserRouter>
-  </Root>
+  <ThemeProvider theme={theme}>
+    <Root config={config} style={{ minHeight: '100vh' }}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/get" component={Home} />
+          <Route path="/connect" component={Connect} />
+          <Route path="/server/websites" component={Connect} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    </Root>
+  </ThemeProvider>
 );
 
 export default App;
